4dc109e74d6678759ef94a5435deea4d417eb0ea
Denitsa Saynova <saynova@alvis1.int.private>
2023-03-20 11:33:14 +0100

    Allow different prompt formats
    Add funsctioannlity for different output formats: atlas, pararel, ernie_zhang
    
    Additional minor changes:
    changed output to jsonl file;
    removed commented out code from original script;
    changed quote chars;

--- pararel/consistency/generate_data.py
+++ pararel/consistency/generate_data.py
@@ -6,9 +6,27 @@ import os
 import json
 
 from pararel.consistency import utils
+from pararel.consistency.lm_pipeline import parse_prompt
 
 
-def generate_data(num_relations, num_tuples, relations_given, LAMA_path):
+def get_pararel_prompt(sample, prompt):
+   return {'prompt': parse_prompt(prompt, sample["sub_label"], "[MASK]"),
+                                'sub_label': sample["sub_label"], 'obj_label': sample["obj_label"]}
+
+def get_atlas_prompt(sample, prompt):
+    return {'question': parse_prompt(prompt, sample["sub_label"], "<extra_id_0>"),
+                     'sub_label': sample["sub_label"], 'answers': [sample["obj_label"]],
+                     'pattern': prompt}
+
+def get_ernie_zhang_prompt(sample, prompt):
+    return {'question': parse_prompt(prompt, sample["sub_label"], "<extra_id_0>"),
+                     'sub_label': sample["sub_label"], 'answers': [sample["obj_label"]],
+                     'pattern': prompt}
+
+POSSIBLE_FORMATS = {"pararel": get_pararel_prompt, "atlas": get_atlas_prompt, "ernie_zhang": get_ernie_zhang_prompt}
+
+
+def generate_data(num_relations, relations_given, LAMA_path, format_prompt):
 
     graph_path = "/mimer/NOBACKUP/groups/dsaynova/SKR/ParaRel/pararel/data/pattern_data/graphs/"
     relations_path = glob.glob(graph_path + "*.graph")
@@ -16,36 +34,27 @@ def generate_data(num_relations, num_tuples, relations_given, LAMA_path):
     if not os.path.exists(output_path):
         os.mkdir(output_path)
 
-    #random.shuffle(relations_path)
     relation_path_keep = []
-    #metadata = "_"
     if relations_given != "":
         relations_given = sorted(relations_given.split(","))
         for relation_path in relations_path:
             relation = relation_path.split("/")[-1].split(".")[0]
             if relation in relations_given:
                 relation_path_keep.append(relation_path)
-        #metadata += "_".join(relations_given)
-        #metadata += "-"
     if len(relation_path_keep) < num_relations:
         for relation_path in relations_path:
             if relation_path not in relation_path_keep:
                 relation = relation_path.split("/")[-1].split(".")[0]
                 relation_path_keep.append(relation_path)
-                #metadata += relation
-                #metadata += "-"
                 if len(relation_path_keep) == num_relations:
                     break
-    #metadata = metadata.strip("-")
-    output_path = output_path + "all_n1/"  #+ str(num_tuples) + "_" + str(num_relations) + metadata + "/"
+    output_path = output_path + "all_n1_" + format_prompt + "/"
 
     if not os.path.exists(output_path):
         print("Saving data to: ", output_path)
         os.mkdir(output_path)
 
         output_path_true = output_path + "train_"
-        #output_path_mlm = output_path + "train_mlm.txt"
-        #f_mlm = open(output_path_mlm, "w")
 
         for relation_path in relation_path_keep:
 
@@ -53,51 +62,40 @@ def generate_data(num_relations, num_tuples, relations_given, LAMA_path):
                 graph = pickle.load(f)
             relation = relation_path.split("/")[-1].split(".")[0]
 
-            f_true = open(output_path_true + relation + ".txt", "w")
+            f_true = open(output_path_true + relation + ".jsonl", "w")
 
             data = utils.read_jsonl_file(LAMA_path + relation + ".jsonl")
-            #random.shuffle(data)
 
             for i, d in enumerate(data):
-                #random.shuffle(data)
                 for node in graph.nodes():
                     pattern = node.lm_pattern
 
-                    pattern = pattern.replace("[X]", d["sub_label"])
-                    pattern = pattern.replace("[Y]", "[MASK]")
-                    #pattern_mlm = pattern.replace("[MASK]", d["obj_label"])
+                    dict_results = POSSIBLE_FORMATS[format_prompt](d, pattern)
 
-                    f_true.write(json.dumps({"query":pattern, "obj_label":d["obj_label"],
-                        "sub_label":d["sub_label"], "pattern":node.lm_pattern}))
+                    f_true.write(json.dumps(dict_results))
                     f_true.write("\n")
-                    #f_mlm.write(pattern_mlm)
-                    #f_mlm.write("\n")
-
-                #f_true.write("\n")
-
-                #if i >= num_tuples:
-                #    break
 
             f_true.close()
 
-        #f_mlm.close()
     else:
         print("Data already exists")
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--num_relations', '-nr', type=int, default=3, help='number of relations')
-    parser.add_argument('--num_tuples', '-nt', type=int, default=100, help='number of tuples')
-    parser.add_argument('--relations_given', '-r', type=str, default="P138,P449,P37", help='which relations')
-    parser.add_argument('--LAMA_path', '-lama', type=str,
-                        default="/mimer/NOBACKUP/groups/dsaynova/SKR/ParaRel/pararel/data/trex_lms_vocab/", help='number of tuples')
+    parser = argparse.ArgumentParser("")
+    parser.add_argument("--num_relations", "-nr", type=int, default=3, help="number of relations")
+    parser.add_argument("-relations_given", "-r", type=str, default="P138,P449,P37", help="which relations")
+    parser.add_argument("--LAMA_path", "-lama", type=str,
+                        default="/mimer/NOBACKUP/groups/dsaynova/SKR/ParaRel/pararel/data/trex_lms_vocab/", help="number of tuples")
+    parser.add_argument("--format", type=str, help="format to match for pararel queries", default="pararel")
 
     args = parser.parse_args()
 
-    generate_data(args.num_relations, args.num_tuples, args.relations_given, args.LAMA_path)
+    if args.format not in POSSIBLE_FORMATS:
+        raise ValueError(f"This function does not yet have support for any other formats than {POSSIBLE_FORMATS}.")
+
+    generate_data(args.num_relations, args.relations_given, args.LAMA_path, args.format)
 
 
 if __name__ == "__main__":
     main()
-
